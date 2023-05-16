598c43713c5063ffe4aade760b16bc57561d8394
Alex Groom <alex.groom@gmail.com>
2023-05-16 15:14:37 +0100

    updates

--- htpasswd/updatehtpasswd.sh
+++ htpasswd/updatehtpasswd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-oc get secret htpasswd-secret -ojsonpath={.data.htpasswd} -n openshift-config | base64 --decode > users.htpasswd
+oc get secret htpasswd-secret -ojsonpath={.data.htpasswd} -n openshift-config | base64 --decode | grep -e admin -e karla > users.htpasswd
 curl https://raw.githubusercontent.com/RedHat-EMEA-SSA-Team/workshop-operator/2.4/htpasswd/htpasswd >> users.htpasswd
 oc create secret generic htpasswd-secret --from-file=htpasswd=users.htpasswd --dry-run=client -o yaml -n openshift-config | oc replace -f -
 oc adm policy add-cluster-role-to-user cluster-admin opentlc-mgr
